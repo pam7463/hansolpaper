5ccfaa2363088b1e44ff21166aefd7d157a43cb9
pam7463 <67131974+pam7463@users.noreply.github.com>
2020-11-03 11:42:44 +0900

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,9 @@
+// 스킵메뉴 호출
+var skipMenu = $('.skip_menu');
+var skipNav = $('.skip_menu>a');
+skipNav.focus(function(){
+  skipMenu.addClass('active');
+});
+skipNav.blur(function(){
+  skipMenu.removeClass('active');
+});
