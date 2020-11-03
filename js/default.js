34ba317b588ddd89330250a17f21759523d1fa60
pam7463 <67131974+pam7463@users.noreply.github.com>
2020-11-03 13:29:50 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -11,4 +11,25 @@ skipNav.blur(function(){
 // 토글메뉴
 $('.tgl_btn').on('click',function(){
   $(this).toggleClass('active');
+  if($(this).hasClass('active')){
+    $('.nav').css('right',0);
+  }else{
+    $('.nav').css('right','-100%');
+  }
+});
+
+// 랭귀지오버
+$('.lang>a').on('click',function(){
+  $('.lang>a').removeClass('active');
+  $(this).addClass('active');
+});
+
+// 메인네비 클릭
+$('.main_nav>li>a').on('click',function(){
+  $('.main_nav>li>a').removeClass('active');
+  $('.main_nav>li>a').next().css('display','none');
+  $(this).addClass('active');
+  $(this).next().css('display','block');
+
 })
+
